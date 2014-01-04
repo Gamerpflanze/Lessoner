3d1288b7567c1166d3360d6c9f78222c8932408d
Gamerpflanze <f.fuerstenberg@hotmail.de>
2014-01-04 19:28:33 +0100

    Hinzufügen von Dateien
    Bootstrap, Jquery etc hinzugefügt

---
+++ Lessoner/Lessoner/Javascript/LoginScript.js
@@ -0,0 +1,39 @@
+﻿/// <reference path="../JQuery/jquery-1.10.2.js" />
+/// <reference path="../Bootstrap/js/bootstrap.js" />
+
+
+function SendLoginData()
+{
+    var Username = jQuery("#Username").val();
+    var Passwort = jQuery("#Password").val();
+
+    if(Username==""||Passwort=="")
+    {
+        //Fehlermeldung hier
+        var Error = jQuery("<b></b>");
+        Error.text("Fehler");
+        Error.appendTo("#maincontainer");
+    }
+    else
+    {
+        $.ajax({
+            type: "POST",
+            url: "Default.aspx/GetLoginData",
+            async:false,
+            contentType: "application/json; charset=utf-8;",
+            dataType:"json",
+            data: JSON.stringify({'user':Username, 'pass':Passwort}),
+            success: function (data) { LoginRecieve(data) },
+            error: function (message) { AjaxError(message) }
+        });
+    }
+}
+
+function LoginRecieve(ID)
+{
+}
+
+function AjaxError(message)
+{
+    alert("error");
+}
